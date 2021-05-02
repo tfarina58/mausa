4110697d8e7b8af81458acd4756e7a159fc6e081
RG <renco.grabar@gmail.com>
2021-05-02 09:36:09 +0200

    farina

--- kurac.py
+++ kurac.py
@@ -44,4 +44,4 @@ print("new line ---------------------------------\n")
 score = accuracy_score(y_test, yhat)
 print("new line ---------------------------------\n")
 # summarize score
-print('Accuracy: %.3f' % (score*100))
+print('Accuracjjjy: %.3f' % (score*100))
